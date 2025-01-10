4942bcd6d89b91d8e12c0d48a84b92e2470d9691
patrickchen8 <patrick.chen0459@gmail.com>
2025-01-10 13:16:59 -0600

    added log in

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,12 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 
 const LandingPage = () => {
-    // const [username, setUsername] = 
-    const navigate = useNavigate();
-
-    const handleSignInClick = () => {
-        navigate('/home');
-    }
 
     return (
         <div className="w-screen h-screen bg-purple-100 flex flex-col justify-center items-center gap-4">
@@ -18,7 +12,7 @@ const LandingPage = () => {
             </span>
             <button 
                 className="bg-purple-500 rounded-lg px-2 py-2 font-semibold text-white text-lg" 
-                onClick={() => handleSignInClick()}
+                onClick={signInWithGoogle}
             >
                 Sign in
             </button>
