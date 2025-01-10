7fb5a7ca861dc01bba94c15bed4257f498f5b234
Sachi Parikh <parikhsachi@gmail.com>
2025-01-10 15:11:59 -0600

    Creating OCRComponent

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import LandingPage from '../pages/LandingPage';
 import HomePage from '../pages/HomePage';
 import ReceiptPage from '../pages/ReceiptPage';
+import UploadReceiptPage from "../pages/UploadReceiptPage";
 
 
 const Dispatcher = () => {
@@ -20,6 +21,7 @@ const Dispatcher = () => {
             <Route path="/" element={<LandingPage />} /> 
             <Route path="/home" element={<HomePage />} /> 
             <Route path="/receipt" element={<ReceiptPage {...mockUser}/>} /> 
+            <Route path="/receiptpage" element={<UploadReceiptPage />} /> 
 
         </Routes>
     
