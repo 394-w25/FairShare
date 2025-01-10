e14e4663c8e885a25703035f438dcd66e5f2ec18
mylee2662 <67172710+mylee2662@users.noreply.github.com>
2025-01-10 14:17:56 -0600

    Input field added

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,21 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-const [number, setNumber] = useState('')
 const phone_num = 6314324732
 
-const handleSMS = (phone_num) => {
-    window.location.href = 'sms:+1' + phone_num + '?body=TEST';
-};
+const PaymentPage = () => {
+    const [number, setNumber] = useState('')
 
-const onPhoneNumberChanged = (event) => {
-    if(event.target.value != number){
-        setNumber(event.target.value)
-    }
-}
+    const onPhoneNumberChanged = (event) => {
+        event.target.value != number ? setNumber(event.target.value) : print("Number same")
+    };
 
-const PaymentPage = ({  }) => {
-
+    const handleSMS = (phone_num) => {
+        window.location.href = 'sms:+1' + phone_num + '?body=TEST';
+    };
 
     return (
         <div className="w-screen h-screen bg-purple-100 flex flex-col justify-center items-center gap-4">
@@ -25,7 +22,7 @@ const PaymentPage = ({  }) => {
             <input onChange={onPhoneNumberChanged}></input>
             <button 
                 className="bg-purple-500 rounded-lg px-2 py-2 font-semibold text-white text-lg" 
-                onClick={() => handleSMS(phone_num)}
+                onClick={() => handleSMS(number)}
             >
                 Request Payment
             </button>
