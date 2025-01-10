dc98d5a7091c7691428a1687f34e0f8454aea3ce
Sungmok Will Cho <smcho1010@gmail.com>
2025-01-10 13:11:58 -0600

    landing page ui

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,27 @@
 import { signInWithGoogle } from '../utilities/firebase.js'
+import { useNavigate } from 'react-router-dom';
+
 
 
 const LandingPage = () => {
     // const [username, setUsername] = 
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate('/home');
+    }
 
     return (
-        <div className="">
-            sign in
+        <div className="w-screen h-screen bg-purple-100 flex flex-col justify-center items-center gap-4">
+            <span className="font-semibold text-3xl">
+                FairShare
+            </span>
             <button 
-                className="p-3" 
-                onClick={() => signInWithGoogle()}
+                className="bg-purple-500 rounded-lg px-2 py-2 font-semibold text-white text-lg" 
+                onClick={() => handleSignInClick()}
             >
                 Sign in
             </button>
-
         </div>
     )
 }
