785439e7c500e334b80ea192df78ba693727d8a5
patrickchen8 <patrick.chen0459@gmail.com>
2025-01-10 13:26:02 -0600

    working routing

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -1,18 +1,38 @@
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { createContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate  } from "react-router-dom";
+import { useAuthState } from "../utilities/firebase";
 import LandingPage from '../pages/LandingPage';
 import HomePage from '../pages/HomePage';
 
 
+export const userContext = createContext(); 
+
 const Dispatcher = () => {
+    const [user, loading] = useAuthState(); 
 
-    return (<BrowserRouter> 
+    if(loading) {
+        return <p> loading user </p>
+    }
+
+    return (user ? 
+
+    <BrowserRouter> 
         <Routes>
-            <Route path="/" element={<LandingPage />} /> 
-            <Route path="/home" element={<HomePage />} /> 
-
+            <Route path="/" element={<LadningPage/>}/>
+            <Route path="*" element={ <Navigate to="/"/> } /> 
         </Routes>
+    </BrowserRouter>
     
-    </BrowserRouter>)
+    : 
+    <userContext.Provider value={user}>
+        <BrowserRouter> 
+            <Routes>
+                <Route path="/" element={<HomePage />} /> 
+            </Routes>
+        
+        </BrowserRouter>
+    </userContext.Provider>
+    )
 
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 
 const HomePage = () => {
 
-    return (<p> Home Page</p>); 
+    return (<p> Logged In Page</p>); 
 
 }
 
