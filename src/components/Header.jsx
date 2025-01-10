43b8ed789306f86bc8d02e954d6a0adaca92e5ba
patrickchen8 <patrick.chen0459@gmail.com>
2025-01-10 14:30:44 -0600

    Adds new users to DB

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+
+    return (<div className="flex justify-center">
+        <p className="text-xl font-bold"> FairShare</p>
+    </div>);
+
+}
+
+export default Header; 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,54 @@
-import { signOut } from '../utilities/firebase';
+import { useContext } from 'react'; 
+import Header from '../components/Header';
+import { userContext } from '../components/Dispatcher';
+import { useDbData, useDbUpdate, signOut } from "../utilities/firebase"
+
 
 const HomePage = () => {
+    const user = useContext(userContext);
+    const [data, dataError] = useDbData(`/users/${user.uid}`);
+    const [update, result] = useDbUpdate(`/users/${user.uid}`);
+    const [users, usersError ] = useDbData('/users');
+
+    if (dataError) { 
+        return <div className="flex justify-center items-center"> 
+            <h1 className="text-red-500" >Error loading data {dataError.toString()}</h1> 
+        </div> ;
+    }
+    if (usersError) { 
+        return <div className="flex justify-center items-center"> 
+            <h1 className="text-red-500" >Error loading data {usersError.toString()}</h1> 
+        </div> ;
+    }
+
+
+    if ( data === undefined || users === undefined) {
+        return <div className="flex justify-center items-center"> 
+            <h1>Loading data...</h1> 
+        </div> ;
+    } 
+
+    if (!data) {
+        update({
+            groups: false 
+        })
+    }
+
+    console.log()
+
+
+
 
     return (
-    <button onClick={signOut}> 
-        Sign Out
-    </button>); 
+    <div classNam="flex">
+        <Header />
+
+        <button className="border-1 border-black" onClick={signOut}> Sign Out </button>
+
+
+
+      
+    </div>); 
 
 }
 
