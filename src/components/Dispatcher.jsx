b868b28ab141e9440708953ee75471656df62596
patrickchen8 <patrick.chen0459@gmail.com>
2025-02-02 21:27:44 -0600

    Added in app requesting, Removed all old iMessage code

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -5,25 +5,11 @@ import LandingPage from '../pages/LandingPage';
 import HomePage from '../pages/HomePage';
 import UploadPage from '../pages/UploadPage';
 import ReceiptPage from '../pages/ReceiptPage';
-import UploadReceiptPage from "../pages/UploadReceiptPage";
-import PaymentPage from "../pages/PaymentPage";
-import TestPage from "../pages/TestPage";
 
 
 export const userContext = createContext(); 
 
 const Dispatcher = () => {
-    const mockUser = {
-        item_list: [
-            {id: 1, name: 'salmon roll', price: 12.95, quantity: 3},
-            {id: 2, name:'california roll', price: 8.95, quantity: 2},
-            {id: 3, name:'rice', price: 2.00, quantity: 1},
-            {id: 4, name: 'tuna roll', price: 12.50, quantity: 4}
-        ],
-        mainUser: {name:'Jerry'},
-        people : ['Amy', 'Johnny', 'Katie']
-    };
-
     const [user, loading] = useAuthState(); 
 
     if(loading) {
@@ -38,8 +24,6 @@ const Dispatcher = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} /> 
                 <Route path="/receipt" element={<ReceiptPage />} /> 
-                <Route path="/pay" element={<PaymentPage />} />
-                <Route path="/test" element={<TestPage />} />
                 <Route path="/upload/:groupId" element={<UploadPage />} /> 
             </Routes>
         
