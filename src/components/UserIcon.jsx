7d3b43b3c7fd0bf51af9ce6f2392f9d4147e0f56
patrickchen8 <patrick.chen0459@gmail.com>
2025-02-06 19:30:36 -0600

    Stylized Requests and Added Functionality

--- src/components/UserIcon.jsx
+++ src/components/UserIcon.jsx
@@ -1,9 +1,11 @@
 
-
-const UserIcon = ({ size, email }) => {
+const UserIcon = ({ size, name }) => {
+    const split_name = name.split(" "); 
+    const initials = split_name.length > 1 ? split_name[0][0] + split_name[1][0] : split_name[0][0].toUpperCase()
+    
     return (
     <div className={`flex flex-none justify-center items-center bg-purple-300 w-${size} h-${size} rounded-full self-center`}> 
-        {email[0].toUpperCase()} 
+        {initials} 
     </div>
     );
 }
