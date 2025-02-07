b317209bc14261a973ec589c48bf0e671ecf4786
mylee2662 <67172710+mylee2662@users.noreply.github.com>
2025-02-06 19:53:46 -0600

    Profile Page + User Icon changes

--- src/components/UserIcon.jsx
+++ src/components/UserIcon.jsx
@@ -1,11 +1,15 @@
-
-
 const UserIcon = ({ size, email }) => {
-    return (
-    <div className={`flex flex-none justify-center items-center bg-purple-300 w-${size} h-${size} rounded-full self-center`}> 
-        {email[0].toUpperCase()} 
+  return (
+    <div
+      className={`flex flex-none justify-center items-center bg-purple-300 rounded-full`}
+      style={{
+        width: `${Number(size) / 4}rem`,
+        height: `${Number(size) / 4}rem`,
+      }}
+    >
+      {email[0].toUpperCase()}
     </div>
-    );
-}
+  );
+};
 
-export default UserIcon; 
+export default UserIcon;
